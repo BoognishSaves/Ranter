ac3729a10f7d07425003351c8665ba2218447de6
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-08-29 15:20:31 -0600

    Added index.js to field db variable

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const db = require('../models');
+const db = require('../models');
 const User = require('../models/users.js');
 const Posts = require('../models/posts.js');
 
