c937db98bda34b63b0328d40c3d24b0453fb0a98
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-08-29 10:12:25 -0600

    Connected to Mongo

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Require Modules
 const express = require('express');
-const mongoose = require('mongose');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
+
+// Database Configuration
+const mongoURI = process.env.MONGODB_URI;
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect(mongoURI);
+
+// Connection Error/Success
+db.on('error', (err) => console.log(err.message + ' is Mongo not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+db.on( 'open' , ()=>{
+  console.log('Connection made!');
+});
 
 
 // App Configuration 
