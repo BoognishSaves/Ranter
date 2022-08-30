a00ae33d2e5484c92403d083b4c0cac7731facb5
anthowest <anthowest14@gmail.com>
2022-08-30 09:30:05 -0500

    Add password, edit username min/max, timestamps

--- models/users.js
+++ models/users.js
@@ -3,13 +3,16 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
     name: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true, min: 3, max: 20},
     email: {type: String, required: true, unique: true},
-    image: {type: String, default: 'default.png'},
+    password: {type: String, required: true, min: 6},
+    profileImage: {type: String, default: ''},
     posts: Array,
     followers: Array,
     following: Array,
-});
+},
+{timestamps: true},
+);
 
 const User = mongoose.model('User', userSchema);
 
