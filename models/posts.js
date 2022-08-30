77543601f583394ca3cfec39a9ef7f7c1b9b4c57
anthowest <anthowest14@gmail.com>
2022-08-30 10:26:32 -0500

    Add userID to post schema

--- models/posts.js
+++ models/posts.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema ({
-    username: String,
+    userID: String,
     post: {type: String, required: true, maxLength: 280},
     image: String,
     created: {type: Date, default: Date.now},
